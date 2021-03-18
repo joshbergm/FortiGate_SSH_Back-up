2a577aea4b559f14d640f824bfdc409a991b1aab
Joshua Bergman <josh.bergman@outlook.com>
2021-03-18 13:05:25 +0100

    Update FortiGate_SSH_Back_up.py

--- FortiGate_SSH_Back_up.py
+++ FortiGate_SSH_Back_up.py
@@ -1,18 +1,17 @@
-
-from netmiko import Connecthandler
+from netmiko import ConnectHandler
 import datetime
 
 fw = {'host': '<hostname>',
-           'device_type': 'fortinet',
-           'ip': '<ip>',
-           'username': '<username>',
-           'password': '<password>'}
+      'device_type': 'fortinet',
+      'ip': '<ip>',
+      'username': '<username>',
+      'password': '<password>'}
 
-net_connect = Connecthandler(**fw)
+net_connect = ConnectHandler(**fw)
 output = net_connect.send_command('show full-configuration')
 
 current_time = datetime.datetime.today().strftime('%Y_%m_%d')
 
-with open('<path>' + str(fw['host']) + '_' + str(current_time) + '.cfg', 'w') as f:
+with open('/paste/path/here' + str(fw['host']) + '_' + str(current_time) + '.cfg', 'w') as f:
     for line in output:
         f.write(line)
